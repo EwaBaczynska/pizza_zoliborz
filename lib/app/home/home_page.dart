9b33f36450f2ba6e9a089a2243f75d7c88f9e3b3
EwaBaczynska <baczynska.ewa@gmail.com>
2022-08-15 11:46:36 +0200

    install cloud firestore

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'add_opinion/add_opinion_page_content.dart';
+import 'my_account/my_account_page_content.dart';
+import 'restaurants/restaurants_page_content.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({
     Key? key,
@@ -18,32 +22,19 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Builder(builder: (context) {
+          return const Text('Najlepsze żarło na Żoli');
+        }),
+      ),
       body: Builder(builder: (context) {
         if (currentIndex == 0) {
-          return Center(
-            child: Text('Jeden'),
-          );
+          return const RestaurantPageContent();
         }
         if (currentIndex == 1) {
-          return Center(
-            child: Text('Dwa'),
-          );
+          return const AddOpinionPageContent();
         }
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Jesteś zalogowany jako ${widget.user.email}'),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                child: const Text('Wyloguj'),
-              ),
-            ],
-          ),
-        );
+        return MyAccountPageContent(email: widget.user.email);
       }),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
